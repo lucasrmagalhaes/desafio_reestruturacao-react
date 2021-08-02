d0b4a1dc0b4c0858f6c873ca0833c1e5a64af7eb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-08-02 20:31:48 -0300

    Solved ModalEditFood

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -13,32 +13,42 @@ interface AddFood {
   description: string;
 }
 
-interface ModalAddFoodProps {
-  isOpen: boolean;
-  setIsOpen: () => void;
-  handleAddFood: (data: AddFood) => void;
+interface IFood {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
 }
 
-export default function ModalAddFood({ isOpen, setIsOpen, handleAddFood }: ModalAddFoodProps) {
-  const handleSubmit = async (data: AddFood) => {
-    handleAddFood(data);
-    setIsOpen();
-  };
+interface ModalEditFood {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleUpdateFood: (data: AddFood) => void;
+  editingFood: IFood;
+}
 
+export default function ModalEditFood({ isOpen, setIsOpen, handleUpdateFood, editingFood }: ModalEditFood) {
   const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit = async (data: AddFood) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  }
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
-
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        
         <Input name="image" placeholder="Cole o link aqui" />
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
         <Input name="description" placeholder="Descrição" />
 
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
